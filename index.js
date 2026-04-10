5c5e4b01752d988977767e9964f805a7273d39d4
Md Rifat Bin Sumon <mdsumonpixel@gmail.com>
2026-04-10 20:50:58 +0600

    feat: user creation with duplicate check and admin role update

--- index.js
+++ index.js
@@ -26,8 +26,9 @@ async function run() {
     const menuCollection = client.db("bistro-boss-db").collection("menu");
     const reviewCollection = client.db("bistro-boss-db").collection("reviews");
     const cartCollection = client.db("bistro-boss-db").collection("cart");
+    const userCollection = client.db("bistro-boss-db").collection("user");
 
-    // Get Route For All Menu 
+    // Get Route For All Menu
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -40,25 +41,68 @@ async function run() {
     });
 
     // Post Route For Cart Section
-    app.post('/carts', async(req, res) => {
+    app.post("/carts", async (req, res) => {
       const cartItem = req.body;
-      const result = await cartCollection.insertOne(cartItem)
-      res.send(result)
-    })
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
 
     // Get Route For Cart Section
-    app.get('/carts', async(req, res) => {
+    app.get("/carts", async (req, res) => {
       const result = await cartCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    // Delete a Data From Cart 
-    app.delete('/carts/:id', async(req, res) => {
+    // Delete a Data From Cart
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(filter);
       res.send(result);
-    })
+    });
+
+    // User Data Start
+    // Get Route For User
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Post Route For User
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      if (!user || !user.email) {
+        return res.status(400).send({ message: "Invalid user data" });
+      }
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Update user Data to patch route
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // Delete Route For User
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
